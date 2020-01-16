585e0733dee894914b0f1cdd71b2c79816963faf
norapeach <veganmuffcakes@gmail.com>
2020-01-15 16:31:27 -0800

    add first view via templates/club/index.html

--- Club/views.py
+++ Club/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here:
+# first view is a basic function that returns the rendered index.html page 
+# when requested
+def index (request):
+    return render(request, 'Club/index.html')
+
